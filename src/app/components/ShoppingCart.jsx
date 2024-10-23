e7a26a64390db47599b36a9ceadc2d5762ae1e4b
Hernan Tapia <tapiahernan5@gmail.com>
2024-10-22 19:29:03 -0700

    added some components like Nav, Shopping cart and a button

---
+++ src/app/components/ShoppingCart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+
+const ShoppingCart = () => {
+  return (
+    <div>
+      <AiOutlineShoppingCart />
+    </div>
+  )
+}
+
+export default ShoppingCart
